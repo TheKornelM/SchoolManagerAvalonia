ded618e2b2f506c09c3799c48e64849cb28b9e55
TheKornelM <makari.kornel@gmail.com>
2024-12-30 16:47:21 +0100

    Add classes view prototype

--- SchoolManagerAvalonia/Views/AdminClassesView.axaml.cs
+++ SchoolManagerAvalonia/Views/AdminClassesView.axaml.cs
@@ -1,6 +1,10 @@
+using System.Resources;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaDialogs.Views;
+using SchoolManagerModel.Utils;
+using SchoolManagerViewModel;
 
 namespace SchoolManagerAvalonia;
 
@@ -9,5 +13,35 @@ public partial class AdminClassesView : UserControl
     public AdminClassesView()
     {
         InitializeComponent();
+        DataContext = GetNewClassesViewModel();
+    }
+    
+    private AdminClassesViewModel GetNewClassesViewModel()
+    {
+        var resourceManager = UIResourceFactory.GetNewResource();
+        var classesViewModel = new AdminClassesViewModel();
+        
+        classesViewModel.SuccessfulClassAdd = async () =>
+        {
+            DataContext = GetNewClassesViewModel();
+            SingleActionDialog dialog = new()
+            {
+                Message = resourceManager.GetStringOrDefault("SuccessfullyAdded"),
+                ButtonText = "Ok"
+            };
+
+            await dialog.ShowAsync();
+        };
+        classesViewModel.FailedClassAdd = async (string message) =>
+        {
+            SingleActionDialog dialog = new()
+            {
+                Message = message,
+                ButtonText = "Ok"
+            };
+
+            await dialog.ShowAsync();        
+        };
+        return classesViewModel;
     }
 }
