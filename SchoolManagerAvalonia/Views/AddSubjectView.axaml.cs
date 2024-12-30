f889b5154aa033208e146657c4a3aa3fc7df1069
TheKornelM <126570467+TheKornelM@users.noreply.github.com>
2024-12-30 16:12:33 +0100

    Add subjects (#4)

--- SchoolManagerAvalonia/Views/AddSubjectView.axaml.cs
+++ SchoolManagerAvalonia/Views/AddSubjectView.axaml.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using AvaloniaDialogs.Views;
+using SchoolManagerModel.Utils;
+using SchoolManagerViewModel;
 
 namespace SchoolManagerAvalonia;
 
@@ -9,5 +14,53 @@ public partial class AddSubjectView : UserControl
     public AddSubjectView()
     {
         InitializeComponent();
+        CreateViewModel();
+        LoadViewModelData();
+    }
+    
+    private async void LoadViewModelData()
+    {
+        var viewmodel = GetViewModel();
+        var task1 = viewmodel.LoadClasses();
+        var task2 = viewmodel.LoadTeachers();
+
+        await Task.WhenAll(task1, task2);
+    }
+    
+    private void CreateViewModel()
+    {
+        var resourceManager = UIResourceFactory.GetNewResource();
+        var viewModel = GetViewModel();
+        viewModel.SuccessfulAdd = async () =>
+        {
+            CreateViewModel();
+            LoadViewModelData();
+            
+            SingleActionDialog dialog = new()
+            {
+                Message = resourceManager.GetStringOrDefault("SuccessfullyAdded"),
+                ButtonText = "Ok"
+            };
+
+            await dialog.ShowAsync();
+        };
+        
+        viewModel.FailedAdd = async (string message) =>
+        {
+            SingleActionDialog dialog = new()
+            {
+                Message = message,
+                ButtonText = "Ok"
+            };
+
+            await dialog.ShowAsync();
+        };
+        
+        DataContext = viewModel;
+    }
+
+    private AddSubjectViewModel GetViewModel()
+    {
+        return DataContext as AddSubjectViewModel ?? new AddSubjectViewModel();
     }
 }
